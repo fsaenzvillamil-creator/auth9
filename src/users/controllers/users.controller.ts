10398d611902b79be94722ae1da27b2d552e60f3
Fernando Saenz <fsaenzvillamil@gmail.com>
2026-08-18 17:14:31 -0500

    migracion

--- src/users/controllers/users.controller.ts
+++ src/users/controllers/users.controller.ts
@@ -1,14 +1,42 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ApiBody } from '@nestjs/swagger';
 import { UsersService } from '../services/users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
 
-    constructor(private readonly usersService: UsersService) { }
+  @Get()
+  findAll() {
+    return this.usersService.findAll();
+  }
 
-    @Get()
-    findAll() {
-        return this.usersService.findAll(); // Sin DTOs
-    }
-
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        name: {
+          type: 'string',
+          example: 'Fernando',
+        },
+        email: {
+          type: 'string',
+          example: 'fernando@gmail.com',
+        },
+        password: {
+          type: 'string',
+          example: '123456',
+        },
+        isActive: {
+          type: 'boolean',
+          example: true,
+        },
+      },
+      required: ['name', 'email', 'password'],
+    },
+  })
+  @Post()
+  create(@Body() userData: any) {
+    return this.usersService.create(userData);
+  }
 }
